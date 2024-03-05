193a5191469319de9fea6d6b20e5cdab685b3afb
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-03-05 07:38:42 -0300

    create models, migrations and enums

---
+++ app/Enums/StatusType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum StatusType: string
+{
+    public const STARTED = 'started';
+    public const IN_PROGRESS = 'in_progress';
+    public const DONE = 'done';
+}
